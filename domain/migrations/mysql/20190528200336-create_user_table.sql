2c05c1c115675411f006dd4963becb0042dea092
okto <octoprima93@gmail.com>
2019-05-28 20:27:23 +0700

    Create migration for user and user social

---
+++ domain/migrations/mysql/20190528200336-create_user_table.sql
@@ -0,0 +1,23 @@
+-- +migrate Up
+
+CREATE TABLE IF NOT EXISTS `mark-one`.`users`
+(
+    `id`              INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`            VARCHAR(255) NOT NULL,
+    `username`        VARCHAR(255) NOT NULL,
+    `email`           VARCHAR(255) NOT NULL,
+    `phone`           VARCHAR(255) NOT NULL,
+    `password`        VARCHAR(255) NOT NULL,
+    `last_login`      VARCHAR(255) NOT NULL,
+    `secondary_email` VARCHAR(255) NOT NULL,
+    `is_verified`     TINYINT      NOT NULL,
+    `avatar`          VARCHAR(255) NOT NULL,
+    `created_at`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+-- +migrate Down
+
+DROP TABLE IF EXISTS `mark-one`.`users`;
---
+++ domain/migrations/mysql/20190528201100-create_user_social_table.sql
@@ -0,0 +1,23 @@
+-- +migrate Up
+
+CREATE TABLE IF NOT EXISTS `mark-one`.`user_social`
+(
+    `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `users_id`    INT UNSIGNED NOT NULL,
+    `social_name` VARCHAR(255) NOT NULL,
+    `social_id`   VARCHAR(255) NOT NULL,
+    `created_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    INDEX `fk_user_social_users1_idx` (`users_id` ASC),
+    CONSTRAINT `fk_user_social_users1`
+        FOREIGN KEY (`users_id`)
+            REFERENCES `mark-one`.`users` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
+
+-- +migrate Down
+
+DROP TABLE IF EXISTS `mark-one`.`user_social`;
