c65e2090371f4b632e358edc42a463dd1f0e3da5
okto <octoprima93@gmail.com>
2019-05-30 06:12:38 +0700

    Delete role user method

--- app/context/roleuser/delete_response.go
+++ app/context/roleuser/delete_response.go
@@ -1,1 +1,13 @@
+/*
+ * Copyright (c) 2019
+ * Created at 5/30/19 5:55 AM
+ * Created by oktoprima
+ * Email octoprima93@gmail.com
+ * Github https://github.com/oktopriima
+ */
+
 package roleuser
+
+type deleteResponse struct {
+	Message string `json:"message"`
+}
--- app/context/roleuser/delete_usecase.go
+++ app/context/roleuser/delete_usecase.go
@@ -1,1 +1,34 @@
+/*
+ * Copyright (c) 2019
+ * Created at 5/30/19 5:52 AM
+ * Created by oktoprima
+ * Email octoprima93@gmail.com
+ * Github https://github.com/oktopriima
+ */
+
 package roleuser
+
+type DeleteResponse interface {
+}
+
+type DeleteRequest interface {
+	GetID() int64
+}
+
+func (uc *usecase) Delete(request DeleteRequest) (DeleteResponse, error) {
+	m, err := uc.roleUserRepo.Find(request.GetID())
+	if err != nil {
+		return nil, err
+	}
+
+	tx := uc.db.Begin()
+	if err = uc.roleUserRepo.Delete(m, tx); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+	tx.Commit()
+
+	return deleteResponse{
+		Message: "Success",
+	}, nil
+}
