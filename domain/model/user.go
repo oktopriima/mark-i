9994ebdde54619a9a1da9045aae83c94177ed646
okto <octoprima93@gmail.com>
2019-06-18 09:14:03 +0700

    Add relathionship model

--- domain/model/user.go
+++ domain/model/user.go
@@ -13,16 +13,18 @@ import (
 )
 
 type Users struct {
-	ID             int64     `json:"id"`
-	Name           string    `json:"name"`
-	Username       string    `json:"username"`
-	Email          string    `json:"email"`
-	Phone          string    `json:"phone"`
-	Password       string    `json:"password"`
-	LastLogin      time.Time `json:"last_login"`
-	SecondaryEmail string    `json:"secondary_email"`
-	IsVerified     bool      `json:"is_verified"`
-	Avatar         string    `json:"avatar"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+	ID             int64        `json:"id"`
+	Name           string       `json:"name"`
+	Username       string       `json:"username"`
+	Email          string       `json:"email"`
+	Phone          string       `json:"phone"`
+	Password       string       `json:"password"`
+	LastLogin      time.Time    `json:"last_login"`
+	SecondaryEmail string       `json:"secondary_email"`
+	IsVerified     bool         `json:"is_verified"`
+	Avatar         string       `json:"avatar"`
+	CreatedAt      time.Time    `json:"created_at"`
+	UpdatedAt      time.Time    `json:"updated_at"`
+	Socials        []UserSocial `json:"socials"`
+	RoleUsers      []UserRole   `json:"role_users"`
 }
